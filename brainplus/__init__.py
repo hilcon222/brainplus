873d5ac26d034f60e7a3a9aa91cf4aceb6063949
Etosino <elia.toselli3@gmail.com>
2024-08-10 18:51:25 +0200

    it compiles shit

--- brainplus/__init__.py
+++ brainplus/__init__.py
@@ -1,3 +1,3 @@
 "A compiler for Brainfuck+ esotheric language."
 
-__version__ = "0.0.dev0"
+__version__ = "0.0.dev1"
--- brainplus/brainplus.py
+++ brainplus/brainplus.py
@@ -6,21 +6,40 @@ C_INTRO = """
 
 int main(void) {
 long memory[30000];
+long *p = memory;
 
 """
 
 
-def cname(source):
+def cname(s):
     "Returns the C source file name for a given Brainfuck file."
-    return source.split('.')[0] + ".c"
+    return s.split('.')[0] + ".c"
 
 
 def main(argv=None):
     "Main function"
+    source = ''
     if argv is None:
         argv = sys.argv
     if len(argv) < 2:
         sys.stderr.write("brainplus: too few arguments. Usage: python -m brainplus FILE") # noqa
         sys.exit(1)
+    with open(argv[1], "r", encoding="utf-8") as fin:
+        source = fin.read()
     with open(cname(argv[1]), "w", encoding="utf-8") as fout:
         fout.write(C_INTRO)
+        for char in source:
+            match char:
+                case "<":
+                    fout.write("p--;\n")
+                case ">":
+                    fout.write("p++;\n")
+                case "+":
+                    fout.write("(*p)++;\n")
+                case "-":
+                    fout.write("(*p)--;\n")
+                case ".":
+                    fout.write("putchar(*p);\n")
+                case ";":
+                    fout.write("*p = getchar();\n")
+        fout.write("\n}\n")
