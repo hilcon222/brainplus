227dd41b309ecca2fbb8054bb22d885f0a38bbfa
Etosino <elia.toselli3@gmail.com>
2024-08-10 08:57:58 +0200

    scripts

---
+++ brainplus/__main__.py
@@ -0,0 +1,8 @@
+"Executable callback"
+import sys
+from brainplus import brainplus
+
+
+if __name__ == "__main__":
+    brainplus.main(sys.argv)
+    sys.exit(0)
---
+++ brainplus/brainplus.py
@@ -0,0 +1,9 @@
+"Main module"
+import sys
+
+
+def main(argv=None):
+    "Main function"
+    if argv is None:
+        argv = sys.argv
+    print("do nothing for now!")
