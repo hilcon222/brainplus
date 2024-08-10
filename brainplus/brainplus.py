1abbce16a6677a4fdc94a08b9f1548f313f6308c
Etosino <elia.toselli3@gmail.com>
2024-08-10 17:55:29 +0200

    it kinda does something test

--- brainplus/brainplus.py
+++ brainplus/brainplus.py
@@ -1,9 +1,26 @@
 "Main module"
 import sys
 
+C_INTRO = """
+#include <stdio.h>
+
+int main(void) {
+long memory[30000];
+
+"""
+
+
+def cname(source):
+    "Returns the C source file name for a given Brainfuck file."
+    return source.split('.')[0] + ".c"
+
 
 def main(argv=None):
     "Main function"
     if argv is None:
         argv = sys.argv
-    print("do nothing for now!")
+    if len(argv) < 2:
+        sys.stderr.write("brainplus: too few arguments. Usage: python -m brainplus FILE") # noqa
+        sys.exit(1)
+    with open(cname(argv[1]), "w", encoding="utf-8") as fout:
+        fout.write(C_INTRO)
